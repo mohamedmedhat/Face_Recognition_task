ce3770bd1d9751af67f5db93f14598852a15c1e7
mohamedmedhat <mmr973320@gmail.com>
2024-12-31 19:13:45 +0200

    [fix]: make the gray format 3d like  (48.48.1) => (48,48,3)

--- helper.py
+++ helper.py
@@ -2,13 +2,9 @@ import cv2
 import numpy as np
 
 def processing_img(img):
-    """
-    This function processes a single image or video frame for emotion detection.
-    It resizes it to 48x48 pixels, converts it to grayscale, normalizes, and reshapes for the model.
-    """
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (48, 48))
     img = img / 255.0
-    img = np.expand_dims(img, axis=-1)
-    img = np.expand_dims(img, axis=0)
-    return img
+    img_rgb = np.repeat(img[..., np.newaxis], 3, axis=-1)    
+    img_rgb = np.expand_dims(img_rgb, axis=0)
+    return img_rgb
